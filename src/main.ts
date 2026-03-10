7276fd5ec7ade2ab4878f7803ae4a947c1ca2254
mdz 2999 <mundoalexis1010@gmail.com>
2026-03-09 22:47:16 -0600

    Arreglo Alertas v4

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// Frame busting - Capa adicional de seguridad para prevenir Clickjacking
+if (window.top !== window.self) {
+  window.top.location = window.self.location;
+}
+
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch((err: any) => console.error(err));
